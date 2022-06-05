ac2011cf571717ba9cbf55d9f462b64a77ac37ea
dnors <deno.nors@gmail.com>
2022-06-05 07:34:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, callback) => {
+
+  // Get sorted array based on callback fn
+  let sortedArr = callback(names);
+
+  // Format array
+  let formattedArr = [];
+  for (let item=0; item < sortedArr.length; item++){
+    formattedArr.push((item+1) + ". " + sortedArr[item]);
+  }
+
+  return formattedArr;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.sort().reverse();
+};
+
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
